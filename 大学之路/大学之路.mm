92977ee713ab6dc22a9609da59f0d3e2c68163b2
leosanqing <stormleo@qq.com>
2018-12-23 11:13:45 +0800

    大学之路笔记

---
+++ 大学之路/大学之路.mm
@@ -0,0 +1,409 @@
+<map>
+		<node ID="root" TEXT="大学之路">		<node TEXT="大学起源" ID="39b16731f7c523144" STYLE="bubble" POSITION="right">
+		<node TEXT="古代大学" ID="34516731fa27d404c" STYLE="fork">
+		<node TEXT="有一些神庙传授知识，比如埃及法老神庙，毕达哥拉斯就在那里学习过" ID="24a16731f7dc890f8" STYLE="fork">
+		</node>
+		<node TEXT="之后毕达哥拉斯开始收学徒，并和他们一起学习，生活，类似于剑桥" ID="3516731fad670063" STYLE="fork">
+		</node>
+		<node TEXT="与毕达哥拉斯同时期的孔子也光广收门徒" ID="22716731fa03a619" STYLE="fork">
+		</node>
+		<node TEXT="开始中西方理念差不多，传授大道，然后授业解惑。只是在隋唐科举制度之后，中国的社会氛围就变为了读书是下层就入上层的途径" ID="11516731fb91a30ff" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="近代大学" ID="16d16731fd86a011" STYLE="fork">
+		<node TEXT="最早大学，博洛尼亚大学" ID="2d7167320a0a73083" STYLE="fork">
+		</node>
+		<node TEXT="巴黎大学，两所享&quot;大学之母&quot;美称大学之一" ID="e8167320ac4da038" STYLE="fork">
+		</node>
+		<node TEXT="英法关系僵化，法国驱逐英国学生，他们便在牛津创办学校，。后来学生与当地人不和，便解散了牛津，在剑河创办了剑桥大学。" ID="1df167320c0cf210d" STYLE="fork">
+		</node>
+		<node TEXT="早期西方大学特征" ID="258167320ee00b119" STYLE="fork">
+		<node TEXT="和教会有联系" ID="274167320fd1b7196" STYLE="fork">
+		</node>
+		<node TEXT="坚持精英教育" ID="1c0167321022dc0fd" STYLE="fork">
+		<node TEXT="作为社会的精英，不仅要有学习，有教养，还需要了解历史，并且具有社会责任感和献身精神，同时需要具备管理社会和应付各种复杂情况的本领，因此实行通才教育" ID="10e16732111d8916d" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="早起高等教育目的" ID="3c11673212e3f303d" STYLE="fork">
+		<node TEXT="让教职人员有知识" ID="b16732135dc115b" STYLE="fork">
+		</node>
+		<node TEXT="服务于社会，而不是为了获取名誉或挣大钱" ID="1d616732138daa0f" STYLE="fork">
+		</node>
+		<node TEXT="获得良知" ID="39816732142619094" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="精英教育" ID="11b167373bfc78031" STYLE="bubble" POSITION="right">
+		<node TEXT="西方国家实行的是精英教育，其中的人会在之后回报学校，包括捐款等。" ID="dd167373c4c6716d" STYLE="fork">
+		</node>
+		<node TEXT="很多大学的财政收入(哈佛，斯坦福等)大部分来自于校友的捐款，" ID="31b167373d1fb3132" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="最好的大学" ID="122167373e3a7812e" STYLE="bubble" POSITION="right">
+		<node TEXT="衡量一个大学的好坏很难，衡量大学的教学质量更难" ID="28f167373e5424166" STYLE="fork">
+		</node>
+		<node TEXT="但是当一个大学生从知名大学毕业后，不需要把母校的名字挂在嘴边；当一个学生从一所二三流大学毕业后，那所大学今后会因为出了一个这样的学生而自豪。这样的大学经历就堪称完美了" ID="d4167373ed064126" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="办学理念" ID="371673741223014a" STYLE="bubble" POSITION="right">
+		<node TEXT="好的大学应当有自己的办学理念，自己的特色，结合自己本校的专业而制定相应的策略" ID="3681673c5b8c7013c" STYLE="fork">
+		</node>
+		<node TEXT="好的大学应当有这四方面" ID="d51673c5cb7b113a" STYLE="fork">
+		<node TEXT="首先是培养人才的地方，将那些有潜力、有志向的年轻人培养成对未来社会有所贡献的人" ID="f01673c5d774102b" STYLE="fork">
+		</node>
+		<node TEXT="他是一个研究的中心，引领世界科技的发展，并且对一个国家、一个地区产生积极正面的影响" ID="2611673c5f46f8046" STYLE="fork">
+		</node>
+		<node TEXT="他是一个新思想、新文化的发源地，推动社会的进步。" ID="38a1673c60f2d513" STYLE="fork">
+		</node>
+		<node TEXT="他是年轻人的家，是他们度过人生最好时光的地方" ID="2751673c61d5ea128" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="两类大学，两种高等教育" ID="1fe167418488710b6" STYLE="bubble" POSITION="right">
+		<node TEXT="纽曼式大学" ID="3d316746865f800e4" STYLE="fork">
+		<node TEXT="英国约翰·纽曼" ID="a51674686bd49039" STYLE="fork">
+		<node TEXT="奉行让学生自由学习，不需要老师约束，学生们互相学习。" ID="3ca1674688eee008b" STYLE="fork">
+		</node>
+		<node TEXT="反对知识面窄，缺乏广度的教育；提倡素质教育" ID="1b7167468777a80b5" STYLE="fork">
+		</node>
+		<node TEXT="大行之道。第二个因素" ID="1f2167468a3c9c036" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="代表大学" ID="f1167468948db16f" STYLE="fork">
+		<node TEXT="牛津" ID="3167468968f5052" STYLE="fork">
+		</node>
+		<node TEXT="剑桥" ID="1a5167468d048d032" STYLE="fork">
+		</node>
+		<node TEXT="哈佛" ID="27f167468d20ad006" STYLE="fork">
+		</node>
+		<node TEXT="耶鲁" ID="22167468d3447086" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="洪堡体系" ID="7016746868cb60ac" STYLE="fork">
+		<node TEXT="威廉·冯·洪堡" ID="7b167468e20660c3" STYLE="fork">
+		</node>
+		<node TEXT="实行&quot;研究教学合一&quot;，需要对专业知识特别了解，所以一般为5年" ID="164167468ebbad07d" STYLE="fork">
+		</node>
+		<node TEXT="让德国快速崛起" ID="211167468fa135032" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="美国大学" ID="191674d433c47077" STYLE="fork">
+		<node TEXT="在吉尔曼的和他的弟子怀特，还有怀特的弟子乔丹的带领下，美国建立了一座又一座顶级大学" ID="34d1674d434d4313a" STYLE="fork">
+		</node>
+		<node TEXT="哈佛和斯坦福两个似乎是将纽曼通才教育和洪堡全才教育结合的最好的高校" ID="2071674d447a8c14c" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="剑桥大学" ID="32d167605787950bf" STYLE="bubble" POSITION="right">
+		<node TEXT="成立于1209年" ID="311167605e31a903" STYLE="fork">
+		</node>
+		<node TEXT="相对来说封闭，整个学校周围都是教授学生，少有外来人" ID="390167605d57a8197" STYLE="fork">
+		</node>
+		<node TEXT="特色" ID="1a167605798dd177" STYLE="fork">
+		<node TEXT="院制" ID="161676057a8ea016" STYLE="fork">
+		<node TEXT="每个院都有自己的管理规范，" ID="3cf167605801d002" STYLE="fork">
+		</node>
+		<node TEXT="每个院院长相当于校长，校长不能直接管理学院" ID="b61676058539c125" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="导师制" ID="3271676057dac90ba" STYLE="fork">
+		<node TEXT="不仅指导学业" ID="148167605b8e2f145" STYLE="fork">
+		</node>
+		<node TEXT="还要引导他们成人" ID="1a3167605baef907e" STYLE="fork">
+		</node>
+		<node TEXT="而且导师可能不止一个，你可以向其他导师学习，都不介意" ID="1fe167605cb094115" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="牛津大学" ID="27e167605d07030d2" STYLE="bubble" POSITION="right">
+		<node TEXT="第一任有名有姓的校长" ID="3ab167606d9005171" STYLE="fork">
+		<node TEXT="罗伯特·格罗斯泰斯特" ID="2ab167606d7fa412a" STYLE="fork">
+		</node>
+		<node TEXT="提出系统学习" ID="3ae167606e1811087" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="第一个世界级学者" ID="1c0167606d97c012d" STYLE="fork">
+		<node TEXT="罗杰·培根" ID="d1167606dc65e11b" STYLE="fork">
+		</node>
+		<node TEXT="建立研究所" ID="387167606e5f9b0cb" STYLE="fork">
+		</node>
+		<node TEXT="第一个要修正儒略历的人" ID="7c167606e77020cc" STYLE="fork">
+		</node>
+		<node TEXT="做学问不是为了名利，而是为了纯粹的真理" ID="10d167606f20e215c" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="特色" ID="b4167606fbc25143" STYLE="fork">
+		<node TEXT="人文学科强，被认为是最适合培养政治家的大学" ID="b3167606fc5c70d6" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="英美大学的差异" ID="1a216767486ca0032" STYLE="bubble" POSITION="right">
+		<node TEXT="美国大学生活和中国差不多，而考察方式不同" ID="34f16767492bb909" STYLE="fork">
+		</node>
+		<node TEXT="英国考察方式则和中国差不多" ID="1a61676749d82d108" STYLE="fork">
+		</node>
+		<node TEXT="牛津剑桥和哈佛的差别" ID="62167674a1ca90ed" STYLE="fork">
+		<node TEXT="英国本科研究深度更欠缺" ID="17f167674a78270d9" STYLE="fork">
+		</node>
+		<node TEXT="哈佛培养全才，剑桥牛津还是差一点" ID="32d167674aed4d06d" STYLE="fork">
+		</node>
+		<node TEXT="学生之间，学生和教授之间的交流，美国比英国更加随意广泛" ID="3c4167674b9fb50cd" STYLE="fork">
+		</node>
+		<node TEXT="衡量成绩，美国侧重平时，英国偏重期末" ID="36f167674c2d02058" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="美国私立大学的管理" ID="2d41676757ed5a063" STYLE="bubble" POSITION="right">
+		<node TEXT="教授治校的传统" ID="2fd167675831dd0d1" STYLE="fork">
+		<node TEXT="美国为了保证教授的学术自由，设置了终生教授，但是这个和&quot;铁饭碗&quot;是两个概念" ID="3cb1676758735317f" STYLE="fork">
+		</node>
+		<node TEXT="行政成本低，管理透明" ID="298167675b4bc2043" STYLE="fork">
+		</node>
+		<node TEXT="教授分为助理教授，副教授，终生教授" ID="1f416767593fd50aa" STYLE="fork">
+		</node>
+		<node TEXT="只有终生教授才有权管理学院，而且管理的范围也只到系一级" ID="38a1676759e9fb12b" STYLE="fork">
+		</node>
+		<node TEXT="意义" ID="14a167675b3d77086" STYLE="fork">
+		<node TEXT="不会被政府所干扰，能保证学术自由" ID="26d167675b9cae04e" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="一开始美国的私立大学的学生少，教授少，便由教授在教学之外顺便管理学校，只讲课，不做研究" ID="2111676763e20e134" STYLE="fork">
+		</node>
+		<node TEXT="而且还要为经费担忧，几百英镑就能获得学校的冠名权，比如哈佛，耶鲁。而同期牛顿炒股炒掉了2万英镑" ID="3cf1676765b2f0118" STYLE="fork">
+		</node>
+		<node TEXT="系主任并没有多少实权，而且系主任不会恋栈，干得好继续干，干不好走人。院长也不从系主任里面选取，而且从社会招聘" ID="1ee1676764d8220c6" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="普林斯顿大学" ID="30a167b13bde3b014" STYLE="bubble" POSITION="right">
+		<node TEXT="普林斯顿是最美的三个学校之一" ID="3c6167b13c0e370f3" STYLE="fork">
+		</node>
+		<node TEXT="相对来说文理科更强" ID="202167b13cbc6504c" STYLE="fork">
+		</node>
+		<node TEXT="校园地址" ID="39a167b2bb1292074" STYLE="fork">
+		<node TEXT="偏远" ID="10c167b2baea610ea" STYLE="fork">
+		</node>
+		<node TEXT="因此相对安静，能让人静下心读书" ID="32e167b2bb1e800f9" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="相对来说更容易出领导人，很多总统出自这" ID="152167b2bb6d8b004" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="耶鲁大学" ID="174167b2bbed87097" STYLE="bubble" POSITION="left">
+		<node TEXT="和普林斯顿相似" ID="385167b2bbffc9168" STYLE="fork">
+		</node>
+		<node TEXT="创始人 皮尔逊" ID="3e3167b2bc351f0f2" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="大都市里的常青藤大学" ID="151167b9cfca9913b" STYLE="bubble" POSITION="left">
+		<node TEXT="宾夕法尼亚" ID="2ab167b9d068a00c" STYLE="fork">
+		<node TEXT="地点" ID="ac167b9d0a7910b8" STYLE="fork">
+		<node TEXT="费城" ID="279167b9d0b47115b" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="建校人" ID="165167b9d0c68406c" STYLE="fork">
+		<node TEXT="本杰明·富兰克林" ID="306167b9d13d5c10b" STYLE="fork">
+		</node>
+		<node TEXT="宾夕法尼亚大学继承了富兰克林的价值观和理念–务实" ID="20c167b9d18263067" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="务实" ID="f8167b9d3f1a509f" STYLE="fork">
+		<node TEXT="相对于很多大学，宾夕法尼亚更偏向培养专才。且一开始就差不多选定专业" ID="9d167b9d409a5135" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="哥伦比亚大学" ID="19b167b9d16273004" STYLE="fork">
+		<node TEXT="地点" ID="55167b9d2f64815d" STYLE="fork">
+		<node TEXT="纽约" ID="b167b9d314ad14f" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="特点" ID="1d4167b9d5eb57181" STYLE="fork">
+		<node TEXT="因为地处纽约，所以寸土寸金。" ID="19e167b9d5f3e30f5" STYLE="fork">
+		</node>
+		<node TEXT="没有好的体育馆（之前拟好建造，却出了问题，不了了之)，所以不太适合体育特长生" ID="179167b9d325950b4" STYLE="fork">
+		</node>
+		<node TEXT="难以换专业" ID="380167b9d804c30a2" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="氛围" ID="115167b9d8330204f" STYLE="fork">
+		<node TEXT="相对于普林斯顿和耶鲁，哥伦比亚大学更富有活力" ID="3a3167b9d859f2056" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="哥伦比亚大学和纽约" ID="17b167b9d9ca8c0d5" STYLE="fork">
+		<node TEXT="富有活力，自由" ID="100167b9dabece155" STYLE="fork">
+		</node>
+		<node TEXT="前者和后者的关系不可分割" ID="b3167b9dafeff0d" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="哈佛大学" ID="10e167b9dbcfc805b" STYLE="bubble" POSITION="left">
+		<node TEXT="不是约翰·哈佛创立，只是他冠名" ID="70167b9dbfbc406f" STYLE="fork">
+		</node>
+		<node TEXT="艾略特" ID="3bf167b9dd688b18e" STYLE="fork">
+		<node TEXT="哈佛历史上重要人物，奠定了现在哈佛的基础" ID="d3167b9ddd7a106e" STYLE="fork">
+		</node>
+		<node TEXT="历程" ID="21d167b9e283b105d" STYLE="fork">
+		<node TEXT="将自己仅有的钱用来去欧洲考察，并发表了两篇文章。" ID="135167b9de591a0fc" STYLE="fork">
+		</node>
+		<node TEXT="回来之后，最大的纺织公司想任命他为厂长。但他认为这个只能影响公司，而不能影响美国" ID="3d7167b9e04932163" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="重大改革" ID="3ca167b9e265b30ae" STYLE="fork">
+		<node TEXT="真正开始启动科学研究" ID="358167b9e2b960132" STYLE="fork">
+		</node>
+		<node TEXT="将大学从&quot;以教为主&quot;变成&quot;以学为主&quot;" ID="cb167b9e302d50cb" STYLE="fork">
+		</node>
+		<node TEXT="开设大量课程" ID="1fe167b9e3913e07b" STYLE="fork">
+		</node>
+		<node TEXT="学业采用&quot;集中与分散结合&quot;方式" ID="c3167b9e3a46002a" STYLE="fork">
+		</node>
+		<node TEXT="各学院相对独立" ID="c2167b9e4377a16f" STYLE="fork">
+		</node>
+		<node TEXT="国际化" ID="12a167b9e44cd30f8" STYLE="fork">
+		</node>
+		<node TEXT="帮助少数族裔" ID="2d0167b9e45cc1151" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="遗憾" ID="2bf167b9e4bef007c" STYLE="fork">
+		<node TEXT="没有最好的理工学院" ID="160167b9e4ddf40a" STYLE="fork">
+		</node>
+		<node TEXT="当时差点和&quot;麻省合并&quot;" ID="11c167b9e53596118" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="写作课" ID="16d167b9e2320e134" STYLE="fork">
+		<node TEXT="所有大学都强调写作的重要性。而且哈佛把写作作为必修课" ID="2ef167b9e64f7715" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="最好的理工学院" ID="2d9167bbbc352d133" STYLE="bubble" POSITION="left">
+		<node TEXT="麻省" ID="228167bbbc599e0eb" STYLE="fork">
+		<node TEXT="学生" ID="14f167bbbe7b0617" STYLE="fork">
+		<node TEXT="学生爱恶作剧" ID="8167bbbd675510e" STYLE="fork">
+		</node>
+		<node TEXT="不拘束" ID="2ce167bbbddad101c" STYLE="fork">
+		</node>
+		<node TEXT="科研能力很强，能参与到真正的科研" ID="238167bbbdf0b70df" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="所有课程都会请最好的老师来教，而且都是非常新的课程" ID="27d167bbbe9b010c4" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="加州理工" ID="266167bbbd06c6183" STYLE="fork">
+		<node TEXT="规模小" ID="2e9167bbbf9336199" STYLE="fork">
+		<node TEXT="占地少" ID="1cd167bbbfd9a70b5" STYLE="fork">
+		</node>
+		<node TEXT="人数少（每年收200人）" ID="104167bbc01deb117" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="特点" ID="301167bbc0f36c15f" STYLE="fork">
+		<node TEXT="不是0就是1。（有就做到最好）" ID="1d167bbc1111515d" STYLE="fork">
+		</node>
+		<node TEXT="学生高智商" ID="26b167bbc18f8e041" STYLE="fork">
+		</node>
+		<node TEXT="不像美国大多数大学培养全才，而且培养专才" ID="a167bbc3abb6033" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="斯坦福" ID="2c6167c4291185029" STYLE="bubble" POSITION="left">
+		<node TEXT="硅谷之父" ID="308167c42981bf0e4" STYLE="fork">
+		<node TEXT="弗雷德里克·特曼" ID="44167c438e975107" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="特点" ID="1d9167c43929900e7" STYLE="fork">
+		<node TEXT="全科型大学且各科排名都靠前" ID="20e167c43a19dd0c7" STYLE="fork">
+		</node>
+		<node TEXT="自称是麻省＋哈佛" ID="37c167c43dd005139" STYLE="fork">
+		</node>
+		<node TEXT="更多的愿意创业" ID="12d167c43e216b07b" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="艰辛的发展历程" ID="252167c43a880909a" STYLE="fork">
+		<node TEXT="找校长时，怀特不愿意去偏远地区，便推荐自己的弟子，乔丹去" ID="f7167c43b4dbb111" STYLE="fork">
+		</node>
+		<node TEXT="之后老斯坦福逝世之后，遇到经济危机，财产也被冻结" ID="25f167c43bf555187" STYLE="fork">
+		</node>
+		<node TEXT="学校后来又陷入经济危机" ID="2e0167c43cf96c13" STYLE="fork">
+		</node>
+		<node TEXT="直到允许各个公司租用土地" ID="190167c43d4de1013" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="学校允许教授学生创业，且鼓励他们创业" ID="42167c43da8fa001" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="约翰·霍普金斯" ID="143167c43eda5d01c" STYLE="bubble" POSITION="left">
+		<node TEXT="美国最早的研究型大学" ID="371167c43f99f4068" STYLE="fork">
+		</node>
+		<node TEXT="由约翰·霍普金斯捐赠遗产" ID="10f167c43fdea20ee" STYLE="fork">
+		</node>
+		<node TEXT="从不缺钱" ID="38d167c440614e0f5" STYLE="fork">
+		<node TEXT="不论是最初建校，" ID="3d4167c440894509e" STYLE="fork">
+		</node>
+		<node TEXT="还是后来研究经费" ID="126167c440f7ff115" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="最好的学科" ID="2a4167c44143a4144" STYLE="fork">
+		<node TEXT="医学" ID="238167c441640c178" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="卫斯理学院" ID="281167c4445e38029" STYLE="bubble" POSITION="left">
+		<node TEXT="一所女校" ID="397167c445450d166" STYLE="fork">
+		<node TEXT="且人数很少" ID="1c4167c448c76003e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="误传" ID="2d167c4457012141" STYLE="fork">
+		<node TEXT="宋家三姐妹毕业于这个学院" ID="d3167c4460b34146" STYLE="fork">
+		</node>
+		<node TEXT="只有宋美龄毕业于这个学院" ID="3a2167c44641c1037" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="学院目的" ID="16d167c4466e8618f" STYLE="fork">
+		<node TEXT="最初" ID="2e5167c448588c101" STYLE="fork">
+		<node TEXT="培养太太，为以后的生活准备" ID="261167c446a5fb101" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="后来" ID="2b7167c4485ff518e" STYLE="fork">
+		<node TEXT="思想开放，让学生做研究" ID="2d5167c4486b8e0c5" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="也不缺经费，因为学校人少，虽然经费不是很多" ID="266167c448f2c1038" STYLE="fork">
+		</node>
+		<node TEXT="一般来这里就读的，很多都是中产阶级以上，不必为毕业找工作而愁，可以慢慢找自己的方向" ID="1c0167c449a2a10fe" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="杜克大学" ID="372167c44a6b300c8" STYLE="bubble" POSITION="left">
+		<node TEXT="和斯坦福类似，是一个全面学科的大学" ID="10c167c44ae85601e" STYLE="fork">
+		</node>
+		<node TEXT="科研活跃度仅次于麻省，哈佛，斯坦福" ID="39167c44b6633183" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="华盛顿大学（圣路易斯）" ID="1d7167c44d65e90e6" STYLE="bubble" POSITION="left">
+		<node TEXT="地点" ID="10f167c44ec8e2153" STYLE="fork">
+		<node TEXT="相当于中国的西北" ID="11167c454de6302a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="所以办好一个好大学真的非常难" ID="38d167c455068f096" STYLE="fork">
+		</node>
+		<node TEXT="康普顿效应" ID="153167c4554424025" STYLE="fork">
+		<node TEXT="康普顿发现了康普顿效应，获得诺贝尔奖" ID="334167c455745f12b" STYLE="fork">
+		</node>
+		<node TEXT="从此大学名声鹊起" ID="264167c457322b08a" STYLE="fork">
+		<node TEXT="因此在华盛顿大学，康普顿效应也有另一层含义" ID="97167c457ccdb12c" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="丹佛斯" ID="67167c457826e11d" STYLE="fork">
+		<node TEXT="相当于哈佛的艾略特，奠定了华盛顿大学的基础" ID="152167c45a2d6a017" STYLE="fork">
+		</node>
+		</node>
+		</node>
+</node>
+</map>
